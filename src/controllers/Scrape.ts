72d32986de0be107f4792345550869db6d3b54ad
Anh Vu <anh.vu@vertics.co>
2020-09-05 22:38:44 +0300

    Feat: Implement scraping by puppeteer

--- src/controllers/Scrape.ts
+++ src/controllers/Scrape.ts
@@ -1,16 +1,20 @@
 import { Get, JsonController } from 'routing-controllers'
 import { ExternalApiService } from '../services/ExternalApi'
+import {ScrapeService} from '../services/Scrape'
 
 @JsonController('/scrape')
 export class ScrapeController {
   private ExternalApiService: ExternalApiService
+  private ScrapeService: ScrapeService
   constructor() {
     this.ExternalApiService = new ExternalApiService()
+    this.ScrapeService = new ScrapeService()
   }
 
   @Get('/buy')
   public async scrapingBuyPage() {
     try {
+      return await this.ScrapeService.scrapeBuyPage()
       return this.ExternalApiService.gettingTransactions()
     } catch (e) {
       throw e
