952b4be6c9956c37edf7af73456efe6ed234b1d6
Anh Vu <anh.vu@vertics.co>
2020-09-06 16:31:33 +0300

    Feat: Analyze data

---
+++ src/interfaces/IComparisonInfo.ts
@@ -0,0 +1,13 @@
+/**
+ *
+ * Comparison Info Interface
+ *
+ * @author Anh Vu <vu.haianh291@gmail.com>
+ *
+ * @copyright Anh Vu
+ */
+
+export interface IComparisonInfo {
+	length: number,
+	avg_diff: number,
+}
